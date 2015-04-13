e33b23e1e3f9f850313219423ccd50b1ea66953a
zcunique <zengchen94@gmail.com>
2015-04-13 17:48:05 +0800

    words now chosen from database randomly

---
+++ python2access.py
@@ -0,0 +1,23 @@
+# -*- coding: cp936 -*-
+import win32com.client
+import pypyodbc
+import random
+def randomWords():
+    randomNum = random.randint(1, 15328)
+
+    conn = win32com.client.Dispatch(r'ADODB.Connection')   
+    DSN = 'PROVIDER=Microsoft.Jet.OLEDB.4.0;DATA SOURCE=./EnWords_access.mdb;'   
+    conn.Open(DSN)
+
+    rs = win32com.client.Dispatch(r'ADODB.Recordset')   
+    rs_name = 'Words'
+    rs.Cursorlocation = 3 # don't use parenthesis here   
+    rs.Open('Select * FROM [Words] where ID = '+str(randomNum), conn) # be sure conn is open
+    return rs.Fields.Item(1).Value
+    #rs.Open('Select * FROM [Words]', conn)
+    #print rs.RecordCount # no parenthesis here either
+    #15328
+
+    #conn = pypyodbc.connect('Driver={Microsoft Access Driver (*.mdb)};DBQ=./EnWords_access.mdb')
+    #cur = conn.cursor()
+    #cur.execute('''SELECT * FROM saleout WHERE product_name LIKE '%Huawei%'''')
